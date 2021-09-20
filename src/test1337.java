0ca71db16aa22191e1f5ab54beda201c7695fa95
Seier K <seierk@gmail.com>
2021-09-20 20:09:58 +0200

    hellooo christain added!!

--- src/test1337.java
+++ src/test1337.java
@@ -2,7 +2,7 @@ public class test1337 {
 
     public static void main(String[] args) {
 
-        System.out.println("HEllo CHRistian");
+        System.out.println("HEllo CHRistianasdsad");
 
     }
 }
