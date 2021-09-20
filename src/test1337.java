b6a4f3250f5cff757c80e6040455c069436bebbf
ChristianKjeldgaard <91070423+ChristianKjeldgaard@users.noreply.github.com>
2021-09-20 20:01:54 +0200

    hilsen

--- src/test1337.java
+++ src/test1337.java
@@ -2,5 +2,8 @@ public class test1337 {
 
     public static void main(String[] args) {
 
+
+        System.out.println("hold nu kæft det er op af bakke");
+        System.out.println("Men det er heldigvis snart weekend");
     }
 }
