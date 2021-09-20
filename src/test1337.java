66f85ced0a6494f542a3bee8de826af785564e3a
ChristianKjeldgaard <91070423+ChristianKjeldgaard@users.noreply.github.com>
2021-09-20 20:25:34 +0200

    Prøv lige kig her

--- src/test1337.java
+++ src/test1337.java
@@ -10,5 +10,10 @@ public class test1337 {
         System.out.println("Hold nu kæft");
 
         //kan du lige indsætte en scanner her
+        //JA det kan jeg godt!! :D
+
+        Scanner input = new Scanner(System.in);
+
+       //Hvad skal vi lave???????
     }
 }
